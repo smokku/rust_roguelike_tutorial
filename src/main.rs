b143993e4f7edd5c5013d897c760bd6192085d2b
Tomasz Sterna <tomek@sterna.link>
2020-03-31 01:32:57 +0200

    Hello Rust - RLTK Style!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use rltk::{Console, GameState, Rltk};
+
+struct State {}
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello Rust World");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50()
+        .with_title("Roguelike Tutorial")
+        .build();
+    let gs = State {};
+    rltk::main_loop(context, gs);
 }
