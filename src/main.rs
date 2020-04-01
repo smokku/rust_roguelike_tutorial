5d8211a667c29b00ac41e3ed28062d8232be8d9c
Tomasz Sterna <tomek@sterna.link>
2020-04-01 19:05:36 +0200

    Walking a Map

--- src/main.rs
+++ src/main.rs
@@ -16,15 +16,54 @@ struct Renderable {
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
-struct LeftMover;
+struct Player;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
-struct Player;
+enum TileType {
+    Wall,
+    Floor,
+}
 
 struct State {
     universe: Universe,
     world: World,
+    resources: Resources,
     schedule: Schedule,
+}
+
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    (y as usize * 80) + x as usize
+}
+
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; 80 * 50];
+
+    // Make the boundaries walls
+    for x in 0..80 {
+        map[xy_idx(x, 0)] = TileType::Wall;
+        map[xy_idx(x, 49)] = TileType::Wall;
+    }
+    for y in 0..50 {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(79, y)] = TileType::Wall;
+    }
+
+    // Now we'll randomly splat a bunch of walls.
+    // It won't be pretty, but it's a decent illustration.
+    // First, obtain the thread-local RNG:
+    let mut rng = rltk::RandomNumberGenerator::new();
+
+    let player_idx = xy_idx(40, 25);
+    for _i in 0..400 {
+        let x = rng.roll_dice(1, 79);
+        let y = rng.roll_dice(1, 49);
+        let idx = xy_idx(x, y);
+        if idx != player_idx {
+            map[idx] = TileType::Wall;
+        }
+    }
+
+    map
 }
 
 fn try_move_player(delta_x: i32, delta_y: i32, world: &mut World) {
@@ -50,12 +89,50 @@ fn player_input(gs: &mut State, ctx: &mut Rltk) {
     }
 }
 
+fn draw_map(map: &[TileType], ctx: &mut Rltk) {
+    let mut x = 0;
+    let mut y = 0;
+    for tile in map {
+        // Render a tile depending upon tile type
+        match tile {
+            TileType::Floor => {
+                ctx.set(
+                    x,
+                    y,
+                    RGB::from_f32(0.5, 0.5, 0.5),
+                    RGB::from_f32(0., 0., 0.),
+                    rltk::to_cp437('.'),
+                );
+            }
+            TileType::Wall => {
+                ctx.set(
+                    x,
+                    y,
+                    RGB::from_f32(0.0, 1.0, 0.0),
+                    RGB::from_f32(0., 0., 0.),
+                    rltk::to_cp437('#'),
+                );
+            }
+        }
+
+        // Move the coordinates
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
+        }
+    }
+}
+
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
 
         player_input(self, ctx);
         self.schedule.execute(&mut self.world);
+
+        let map = self.resources.get::<Vec<TileType>>().unwrap();
+        draw_map(&map, ctx);
 
         let query = <(Read<Position>, Read<Renderable>)>::query();
         for (pos, render) in query.iter(&mut self.world) {
@@ -72,6 +149,9 @@ fn main() {
 
     let universe = Universe::new();
     let mut world = universe.create_world();
+    let mut resources = Resources::default();
+
+    resources.insert(new_map());
 
     world.insert(
         (Player,),
@@ -85,36 +165,12 @@ fn main() {
         )],
     );
 
-    world.insert(
-        (LeftMover,),
-        (0..10).map(|i| {
-            (
-                Position { x: i * 7, y: 20 },
-                Renderable {
-                    glyph: rltk::to_cp437('☺'),
-                    fg: RGB::named(rltk::RED),
-                    bg: RGB::named(rltk::BLACK),
-                },
-            )
-        }),
-    );
-
-    let left_walker = SystemBuilder::new("left_walker")
-        .with_query(Write::<Position>::query().filter(tag::<LeftMover>()))
-        .build(|_, mut world, (), query| {
-            for mut pos in query.iter(&mut world) {
-                pos.x -= 1;
-                if pos.x < 0 {
-                    pos.x = 79;
-                }
-            }
-        });
-
-    let schedule = Schedule::builder().add_system(left_walker).build();
+    let schedule = Schedule::builder().build();
 
     let gs = State {
         universe,
         world,
+        resources,
         schedule,
     };
     rltk::main_loop(context, gs);
