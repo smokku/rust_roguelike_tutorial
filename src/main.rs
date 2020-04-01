43d04db1c21d3666aaf96de73b930cf9515045b4
Tomasz Sterna <tomek@sterna.link>
2020-04-01 16:52:59 +0200

    Iterating entities - a generic render system

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,32 @@
-use rltk::{Console, GameState, Rltk};
+use legion::prelude::*;
+use rltk::{Console, GameState, Rltk, VirtualKeyCode, RGB};
 
-struct State {}
+// #[derive(Clone, Copy, Debug, PartialEq)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+// #[derive(Clone, Copy, Debug, PartialEq)]
+struct Renderable {
+    glyph: u8,
+    fg: RGB,
+    bg: RGB,
+}
+
+struct State {
+    universe: Universe,
+    world: World,
+}
+
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
-        ctx.print(1, 1, "Hello Rust World");
+
+        let query = <(Read<Position>, Read<Renderable>)>::query();
+        for (pos, render) in query.iter(&mut self.world) {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
     }
 }
 
@@ -13,6 +35,36 @@ fn main() {
     let context = RltkBuilder::simple80x50()
         .with_title("Roguelike Tutorial")
         .build();
-    let gs = State {};
+
+    let universe = Universe::new();
+    let world = universe.create_world();
+    let mut gs = State { universe, world };
+
+    gs.world.insert(
+        (),
+        vec![(
+            Position { x: 40, y: 25 },
+            Renderable {
+                glyph: rltk::to_cp437('@'),
+                fg: RGB::named(rltk::YELLOW),
+                bg: RGB::named(rltk::BLACK),
+            },
+        )],
+    );
+
+    gs.world.insert(
+        (),
+        (0..10).map(|i| {
+            (
+                Position { x: i * 7, y: 20 },
+                Renderable {
+                    glyph: rltk::to_cp437('☺'),
+                    fg: RGB::named(rltk::RED),
+                    bg: RGB::named(rltk::BLACK),
+                },
+            )
+        }),
+    );
+
     rltk::main_loop(context, gs);
 }
