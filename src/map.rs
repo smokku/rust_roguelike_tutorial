27d4a134f46d8391b5b980282950adad4bf0517a
Tomasz Sterna <tomek@sterna.link>
2020-04-02 20:04:14 +0200

    Chasing the Player

--- src/map.rs
+++ src/map.rs
@@ -47,6 +47,15 @@ impl Map {
             }
         }
     }
+
+    fn is_exit_valid(&self, x: i32, y: i32) -> bool {
+        if x < 1 || x > self.width - 1 || y < 1 || y > self.height - 1 {
+            return false;
+        }
+        let idx = self.xy_idx(x, y);
+        self.tiles[idx] != TileType::Wall
+    }
+
     /// Makes a new map using the algorithm from http://rogueliketutorials.com/tutorials/tcod/part-3/
     /// This gives a handful of random rooms and corridors joining them together.
     pub fn new_map_rooms_and_corridors() -> Self {
@@ -101,6 +110,43 @@ impl Map {
 impl BaseMap for Map {
     fn is_opaque(&self, idx: usize) -> bool {
         self.tiles[idx] == TileType::Wall
+    }
+
+    fn get_available_exits(&self, idx: usize) -> Vec<(usize, f32)> {
+        let mut exits: Vec<(usize, f32)> = Vec::new();
+        let x = idx as i32 % self.width;
+        let y = idx as i32 / self.width;
+        let w = self.width as usize;
+
+        // Cardinal directions
+        if self.is_exit_valid(x - 1, y) {
+            exits.push((idx - 1, 1.0))
+        };
+        if self.is_exit_valid(x + 1, y) {
+            exits.push((idx + 1, 1.0))
+        };
+        if self.is_exit_valid(x, y - 1) {
+            exits.push((idx - w, 1.0))
+        };
+        if self.is_exit_valid(x, y + 1) {
+            exits.push((idx + w, 1.0))
+        };
+
+        // Diagonals
+        if self.is_exit_valid(x - 1, y - 1) {
+            exits.push(((idx - w) - 1, 1.0))
+        };
+        if self.is_exit_valid(x + 1, y - 1) {
+            exits.push(((idx - w) + 1, 1.0))
+        };
+        if self.is_exit_valid(x - 1, y + 1) {
+            exits.push(((idx + w) - 1, 1.0))
+        };
+        if self.is_exit_valid(x + 1, y + 1) {
+            exits.push(((idx + w) + 1, 1.0))
+        };
+
+        exits
     }
 }
 
--- src/monster_ai_system.rs
+++ src/monster_ai_system.rs
@@ -1,15 +1,28 @@
-use super::{Monster, Name, Viewshed};
+use super::{Map, Monster, Name, Position, Viewshed};
 use legion::prelude::*;
 use rltk::{console, Point};
 
 pub fn build() -> std::boxed::Box<(dyn legion::systems::schedule::Schedulable + 'static)> {
     SystemBuilder::new("monster_ai")
+        .write_resource::<Map>()
         .read_resource::<Point>()
-        .with_query(<(Read<Viewshed>, Read<Name>)>::query().filter(tag::<Monster>()))
-        .build(|_, world, player_pos, query| {
-            for (viewshed, name) in query.iter(&world) {
+        .with_query(
+            <(Write<Viewshed>, Read<Name>, Write<Position>)>::query().filter(tag::<Monster>()),
+        )
+        .build(|_, mut world, (map, player_pos), query| {
+            for (mut viewshed, name, mut pos) in query.iter_mut(&mut world) {
                 if viewshed.visible_tiles.contains(&**player_pos) {
                     console::log(&format!("{} shouts insults", name.name));
+                    let path = rltk::a_star_search(
+                        map.xy_idx(pos.x, pos.y) as i32,
+                        map.xy_idx(player_pos.x, player_pos.y) as i32,
+                        &**map,
+                    );
+                    if path.success && path.steps.len() > 1 {
+                        pos.x = path.steps[1] as i32 % map.width;
+                        pos.y = path.steps[1] as i32 / map.width;
+                        viewshed.dirty = true;
+                    }
                 }
             }
         })
