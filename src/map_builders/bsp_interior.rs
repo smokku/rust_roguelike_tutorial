16441b8d074ae45461fc08c3258243c334fee753
Tomasz Sterna <tomek@sterna.link>
2020-05-02 17:45:48 +0200

    Subdividing into rooms

--- src/map_builders/bsp_interior.rs
+++ src/map_builders/bsp_interior.rs
@@ -4,6 +4,8 @@ use super::{
 use legion::prelude::*;
 use rltk::RandomNumberGenerator;
 
+const MIN_ROOM_SIZE: i32 = 8;
+
 pub struct BspInteriorBuilder {
     map: Map,
     starting_position: Position,
@@ -26,7 +28,7 @@ impl MapBuilder for BspInteriorBuilder {
     }
 
     fn build_map(&mut self) {
-        // We should do something here
+        self.build();
     }
 
     fn spawn_entities(&mut self, world: &mut World, resources: &mut Resources) {
@@ -54,6 +56,72 @@ impl BspInteriorBuilder {
             rooms: Vec::new(),
             history: Vec::new(),
             rects: Vec::new(),
+        }
+    }
+
+    fn build(&mut self) {
+        let mut rng = RandomNumberGenerator::new();
+
+        self.rects.clear();
+        self.rects
+            .push(Rect::new(1, 1, self.map.width - 1, self.map.height - 1)); // Start with a single map-sized rectangle
+        self.partition_rects(&mut rng);
+
+        for r in self.rects.clone().iter() {
+            let room = *r;
+            self.rooms.push(room);
+            for y in room.y1..room.y2 {
+                for x in room.x1..room.x2 {
+                    let idx = self.map.xy_idx(x, y);
+                    if idx > 0 && idx < ((self.map.width * self.map.height) - 1) as usize {
+                        self.map.tiles[idx] = TileType::Floor;
+                    }
+                }
+            }
+            self.take_snapshot();
+        }
+
+        let start = self.rooms[0].center();
+        self.starting_position = Position {
+            x: start.0,
+            y: start.1,
+        };
+    }
+
+    fn partition_rects(&mut self, rng: &mut RandomNumberGenerator) {
+        if let Some(rect) = self.rects.pop() {
+            // Calculate boundaries
+            let width = rect.x2 - rect.x1;
+            let height = rect.y2 - rect.y1;
+
+            let split = rng.roll_dice(1, 4);
+            if split <= 2 {
+                // Horizontal split
+                let half_width = width / 2;
+                let h1 = Rect::new(rect.x1, rect.y1, half_width - 1, height);
+                self.rects.push(h1);
+                if half_width > MIN_ROOM_SIZE {
+                    self.partition_rects(rng);
+                }
+                let h2 = Rect::new(rect.x1 + half_width, rect.y1, half_width, height);
+                self.rects.push(h2);
+                if half_width > MIN_ROOM_SIZE {
+                    self.partition_rects(rng);
+                }
+            } else {
+                // Vertical split
+                let half_height = height / 2;
+                let v1 = Rect::new(rect.x1, rect.y1, width, half_height - 1);
+                self.rects.push(v1);
+                if half_height > MIN_ROOM_SIZE {
+                    self.partition_rects(rng);
+                }
+                let v2 = Rect::new(rect.x1, rect.y1 + half_height, width, half_height);
+                self.rects.push(v2);
+                if half_height > MIN_ROOM_SIZE {
+                    self.partition_rects(rng);
+                }
+            }
         }
     }
 }
