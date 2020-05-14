b21b2185c69c1522e4eb44f55eb1ce1e1cdcc2b6
Tomasz Sterna <tomek@sterna.link>
2020-05-14 23:53:11 +0200

    Populating the test map with prefabbed entities

--- src/map_builders/prefab_builder.rs
+++ src/map_builders/prefab_builder.rs
@@ -15,6 +15,7 @@ pub struct PrefabBuilder {
     starting_position: Position,
     history: Vec<Map>,
     mode: PrefabMode,
+    spawns: Vec<(usize, String)>,
 }
 
 impl MapBuilder for PrefabBuilder {
@@ -34,7 +35,11 @@ impl MapBuilder for PrefabBuilder {
         self.build();
     }
 
-    fn spawn_entities(&mut self, world: &mut World, resources: &mut Resources) {}
+    fn spawn_entities(&mut self, world: &mut World, resources: &mut Resources) {
+        for (idx, name) in self.spawns.iter() {
+            spawner::spawn_entity(world, idx, name);
+        }
+    }
 
     fn take_snapshot(&mut self) {
         if SHOW_MAPGEN_VISUALIZER {
@@ -54,8 +59,9 @@ impl PrefabBuilder {
             starting_position: Position { x: 0, y: 0 },
             history: Vec::new(),
             mode: PrefabMode::RexLevel {
-                template: "../resources/wfc-demo1.xp",
+                template: "../resources/wfc-populated.xp",
             },
+            spawns: Vec::new(),
         }
     }
 
@@ -63,10 +69,24 @@ impl PrefabBuilder {
         match self.mode {
             PrefabMode::RexLevel { template } => self.load_rex_map(template),
         }
-
-        // Set a central starting point
-        self.starting_position = get_central_starting_position(&self.map);
         self.take_snapshot();
+
+        if self.starting_position.x == 0 {
+            // Set a central starting point
+            self.starting_position = get_central_starting_position(&self.map);
+
+            // Find all tiles we can reach from the starting point
+            let start_idx = self
+                .map
+                .xy_idx(self.starting_position.x, self.starting_position.y);
+            let exit_tile =
+                remove_unreachable_areas_returning_most_distant(&mut self.map, start_idx);
+            self.take_snapshot();
+
+            // Place the stairs
+            self.map.tiles[exit_tile] = TileType::DownStairs;
+            self.take_snapshot();
+        }
     }
 
     fn load_rex_map(&mut self, path: &str) {
@@ -81,7 +101,40 @@ impl PrefabBuilder {
                         match (cell.ch as u8) as char {
                             ' ' => self.map.tiles[idx] = TileType::Floor,
                             '#' => self.map.tiles[idx] = TileType::Wall,
-                            _ => {}
+                            '>' => self.map.tiles[idx] = TileType::DownStairs,
+
+                            '@' => {
+                                self.map.tiles[idx] = TileType::Floor;
+                                self.starting_position = Position {
+                                    x: x as i32,
+                                    y: y as i32,
+                                };
+                            }
+
+                            'g' => {
+                                self.map.tiles[idx] = TileType::Floor;
+                                self.spawns.push((idx, "Goblin".to_string()));
+                            }
+                            'o' => {
+                                self.map.tiles[idx] = TileType::Floor;
+                                self.spawns.push((idx, "Orc".to_string()));
+                            }
+                            '^' => {
+                                self.map.tiles[idx] = TileType::Floor;
+                                self.spawns.push((idx, "Bear Trap".to_string()));
+                            }
+                            '%' => {
+                                self.map.tiles[idx] = TileType::Floor;
+                                self.spawns.push((idx, "Rations".to_string()));
+                            }
+                            '!' => {
+                                self.map.tiles[idx] = TileType::Floor;
+                                self.spawns.push((idx, "Health Potion".to_string()));
+                            }
+
+                            c => {
+                                rltk::console::log(format!("Unknown glyph loading map: {}", c));
+                            }
                         }
                     }
                 }
