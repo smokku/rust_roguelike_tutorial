8339a8a99e7d9f3781044e865120f5ee7a7f99b4
Tomasz Sterna <tomek@sterna.link>
2020-05-20 09:59:53 +0200

    Updating the Maze Builder

--- src/map_builders/maze.rs
+++ src/map_builders/maze.rs
@@ -1,98 +1,27 @@
-use super::{
-    generate_voronoi_spawn_regions, remove_unreachable_areas_returning_most_distant, spawner, Map,
-    MapBuilder, Position, TileType, SHOW_MAPGEN_VISUALIZER,
-};
+use super::{BuilderMap, InitialMapBuilder, Map, TileType};
 use rltk::RandomNumberGenerator;
-use std::collections::HashMap;
 
-pub struct MazeBuilder {
-    map: Map,
-    starting_position: Position,
-    history: Vec<Map>,
-    noise_areas: HashMap<i32, Vec<usize>>,
-    spawn_list: Vec<(usize, String)>,
-}
+pub struct MazeBuilder {}
 
-impl MapBuilder for MazeBuilder {
-    fn get_map(&self) -> Map {
-        self.map.clone()
-    }
-
-    fn get_starting_position(&self) -> Position {
-        self.starting_position.clone()
-    }
-
-    fn get_snapshot_history(&self) -> Vec<Map> {
-        self.history.clone()
-    }
-
-    fn build_map(&mut self) {
-        self.build();
-    }
-
-    fn get_spawn_list(&self) -> &Vec<(usize, String)> {
-        &self.spawn_list
-    }
-    fn take_snapshot(&mut self) {
-        if SHOW_MAPGEN_VISUALIZER {
-            let mut snapshot = self.map.clone();
-            for v in snapshot.revealed_tiles.iter_mut() {
-                *v = true;
-            }
-            self.history.push(snapshot);
-        }
+impl InitialMapBuilder for MazeBuilder {
+    fn build_map(&mut self, rng: &mut RandomNumberGenerator, build_data: &mut BuilderMap) {
+        self.build(rng, build_data);
     }
 }
 
 impl MazeBuilder {
-    pub fn new(depth: i32) -> Self {
-        MazeBuilder {
-            map: Map::new(depth),
-            starting_position: Position { x: 0, y: 0 },
-            history: Vec::new(),
-            noise_areas: HashMap::new(),
-            spawn_list: Vec::new(),
-        }
+    pub fn new() -> Box<Self> {
+        Box::new(MazeBuilder {})
     }
 
-    fn build(&mut self) {
-        let mut rng = RandomNumberGenerator::new();
-
+    fn build(&mut self, rng: &mut RandomNumberGenerator, build_data: &mut BuilderMap) {
         // Maze gen
         let mut maze = Grid::new(
-            (self.map.width / 2) - 2,
-            (self.map.height / 2) - 2,
-            &mut rng,
+            (build_data.map.width / 2) - 2,
+            (build_data.map.height / 2) - 2,
+            rng,
         );
-        maze.generate_maze(self);
-
-        // Set starting point
-        self.starting_position = Position { x: 2, y: 2 };
-
-        // Find all tiles we can reach from the starting point
-        let start_idx = self
-            .map
-            .xy_idx(self.starting_position.x, self.starting_position.y);
-        let exit_tile = remove_unreachable_areas_returning_most_distant(&mut self.map, start_idx);
-        self.take_snapshot();
-
-        // Place the stairs
-        self.map.tiles[exit_tile] = TileType::DownStairs;
-        self.take_snapshot();
-
-        // Now we build a noise map for use in spawning entities later
-        self.noise_areas = generate_voronoi_spawn_regions(&self.map, &mut rng);
-
-        // Spawn the entities
-        for (_id, area) in self.noise_areas.iter() {
-            spawner::spawn_region(
-                &self.map,
-                &mut rng,
-                area,
-                self.map.depth,
-                &mut self.spawn_list,
-            );
-        }
+        maze.generate_maze(build_data);
     }
 }
 
@@ -112,7 +41,7 @@ struct Cell {
 }
 
 impl Cell {
-    pub fn new(row: i32, column: i32) -> Self {
+    fn new(row: i32, column: i32) -> Self {
         Cell {
             row,
             column,
@@ -209,7 +138,7 @@ impl<'a> Grid<'a> {
         }
     }
 
-    fn generate_maze(&mut self, generator: &mut MazeBuilder) {
+    fn generate_maze(&mut self, build_data: &mut BuilderMap) {
         let mut i = 0;
         loop {
             self.cells[self.current].visited = true;
@@ -235,8 +164,8 @@ impl<'a> Grid<'a> {
             }
 
             if i % 50 == 0 {
-                self.copy_to_map(&mut generator.map);
-                generator.take_snapshot();
+                self.copy_to_map(&mut build_data.map);
+                build_data.take_snapshot();
             }
             i += 1;
         }
