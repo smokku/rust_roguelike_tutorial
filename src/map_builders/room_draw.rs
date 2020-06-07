5c973abe55e4ad80303b9697082803b0b73ad1ed
Tomasz Sterna <tomek@sterna.link>
2020-06-07 23:11:31 +0200

    Circular Rooms

--- src/map_builders/room_draw.rs
+++ src/map_builders/room_draw.rs
@@ -1,4 +1,4 @@
-use super::{BuilderMap, MetaMapBuilder, TileType};
+use super::{BuilderMap, MetaMapBuilder, Rect, TileType};
 use rltk::RandomNumberGenerator;
 
 pub struct RoomDrawer {}
@@ -14,6 +14,35 @@ impl RoomDrawer {
         Box::new(RoomDrawer {})
     }
 
+    fn rectangle(&mut self, build_data: &mut BuilderMap, room: &Rect) {
+        for y in room.y1 + 1..=room.y2 {
+            for x in room.x1 + 1..=room.x2 {
+                let idx = build_data.map.xy_idx(x, y);
+                if idx < build_data.map.width as usize * build_data.map.height as usize {
+                    build_data.map.tiles[idx] = TileType::Floor;
+                }
+            }
+        }
+    }
+
+    fn circle(&mut self, build_data: &mut BuilderMap, room: &Rect) {
+        let radius = i32::min(room.x2 - room.x1, room.y2 - room.y1) as f32 / 2.0;
+        let center = room.center();
+        let center_pt = rltk::Point::new(center.0, center.1);
+        for y in room.y1..=room.y2 {
+            for x in room.x1..=room.x2 {
+                let idx = build_data.map.xy_idx(x, y);
+                let distance =
+                    rltk::DistanceAlg::Pythagoras.distance2d(center_pt, rltk::Point::new(x, y));
+                if idx < (build_data.map.width * build_data.map.height) as usize
+                    && distance <= radius
+                {
+                    build_data.map.tiles[idx] = TileType::Floor;
+                }
+            }
+        }
+    }
+
     fn build(&mut self, rng: &mut RandomNumberGenerator, build_data: &mut BuilderMap) {
         let rooms = if let Some(rooms_builder) = &build_data.rooms {
             rooms_builder.clone()
@@ -22,14 +51,7 @@ impl RoomDrawer {
         };
 
         for room in rooms.iter() {
-            for y in room.y1 + 1..=room.y2 {
-                for x in room.x1 + 1..=room.x2 {
-                    let idx = build_data.map.xy_idx(x, y);
-                    if idx < build_data.map.width as usize * build_data.map.height as usize {
-                        build_data.map.tiles[idx] = TileType::Floor;
-                    }
-                }
-            }
+            self.circle(build_data, room);
             build_data.take_snapshot();
         }
     }
