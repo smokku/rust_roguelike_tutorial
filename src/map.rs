dc96ebea1661809b3bbe09801d7484df74fdb8d4
Tomasz Sterna <tomek@sterna.link>
2020-06-11 00:59:01 +0200

    Revisiting draw_map for progressive map rendering

--- src/map.rs
+++ src/map.rs
@@ -1,5 +1,5 @@
 use legion::prelude::*;
-use rltk::{Algorithm2D, BaseMap, Point, Rltk, SmallVec, RGB};
+use rltk::{Algorithm2D, BaseMap, Point, SmallVec};
 use serde::{Deserialize, Serialize};
 use std::collections::HashSet;
 use type_uuid::TypeUuid;
@@ -118,95 +118,5 @@ impl BaseMap for Map {
 impl Algorithm2D for Map {
     fn dimensions(&self) -> Point {
         Point::new(self.width, self.height)
-    }
-}
-
-fn is_revealed_and_wall(map: &Map, x: i32, y: i32) -> bool {
-    if x < 0 || x >= map.width || y < 0 || y >= map.height {
-        return false;
-    }
-    let idx = map.xy_idx(x, y);
-    map.tiles[idx] == TileType::Wall && map.revealed_tiles[idx]
-}
-
-fn wall_glyph(map: &Map, x: i32, y: i32) -> rltk::FontCharType {
-    if x < 0 || x >= map.width || y < 0 || y >= map.height {
-        return 35;
-    }
-    let mut mask: u8 = 0;
-
-    if is_revealed_and_wall(map, x, y - 1) {
-        mask += 1;
-    }
-    if is_revealed_and_wall(map, x, y + 1) {
-        mask += 2;
-    }
-    if is_revealed_and_wall(map, x - 1, y) {
-        mask += 4;
-    }
-    if is_revealed_and_wall(map, x + 1, y) {
-        mask += 8;
-    }
-
-    match mask {
-        0 => 9,    // Pillar because we can't see neighbors
-        1 => 186,  // Wall only to the north
-        2 => 186,  // Wall only to the south
-        3 => 186,  // Wall to the north and south
-        4 => 205,  // Wall only to the west
-        5 => 188,  // Wall to the north and west
-        6 => 187,  // Wall to the south and west
-        7 => 185,  // Wall to the north, south and west
-        8 => 205,  // Wall only to the east
-        9 => 200,  // Wall to the north and east
-        10 => 201, // Wall to the south and east
-        11 => 204, // Wall to the north, south and east
-        12 => 205, // Wall to the east and west
-        13 => 202, // Wall to the east, west, and south
-        14 => 203, // Wall to the east, west, and north
-        15 => 206, // ╬ Wall on all sides
-        _ => 35,   // We missed one?
-    }
-}
-
-pub fn draw_map(map: &Map, ctx: &mut Rltk) {
-    let mut x = 0;
-    let mut y = 0;
-    for (idx, tile) in map.tiles.iter().enumerate() {
-        // Render a tile depending upon tile type
-        if map.revealed_tiles[idx] {
-            let glyph;
-            let mut fg;
-            let mut bg = RGB::from_f32(0., 0., 0.);
-            match tile {
-                TileType::Floor => {
-                    glyph = rltk::to_cp437('.');
-                    fg = RGB::from_f32(0.0, 0.5, 0.5);
-                }
-                TileType::Wall => {
-                    glyph = wall_glyph(&map, x, y);
-                    fg = RGB::from_f32(0.0, 1.0, 0.0);
-                }
-                TileType::DownStairs => {
-                    glyph = rltk::to_cp437('>');
-                    fg = RGB::from_f32(0.0, 1.0, 1.0);
-                }
-            }
-            if map.visible_tiles[idx] {
-                if map.bloodstains.contains(&idx) {
-                    bg = RGB::from_f32(0.75, 0.0, 0.0);
-                }
-            } else {
-                fg = fg.to_greyscale()
-            }
-            ctx.set(x, y, fg, bg, glyph);
-        }
-
-        // Move the coordinates
-        x += 1;
-        if x > map.width - 1 {
-            x = 0;
-            y += 1;
-        }
     }
 }
