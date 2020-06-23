9436d7c724b37d53806a141580290e47600c367a
Tomasz Sterna <tomek@sterna.link>
2020-06-23 12:31:25 +0200

    Making some new tile types

--- src/camera.rs
+++ src/camera.rs
@@ -127,6 +127,10 @@ fn get_tile_glyph(idx: usize, map: &Map) -> (rltk::FontCharType, RGB, RGB) {
             glyph = rltk::to_cp437('.');
             fg = RGB::from_f32(0.0, 0.5, 0.5);
         }
+        TileType::WoodFloor => {
+            glyph = rltk::to_cp437('.');
+            fg = RGB::named(rltk::CHOCOLATE);
+        }
         TileType::Wall => {
             let x = idx as i32 % map.width;
             let y = idx as i32 / map.width;
@@ -136,6 +140,26 @@ fn get_tile_glyph(idx: usize, map: &Map) -> (rltk::FontCharType, RGB, RGB) {
         TileType::DownStairs => {
             glyph = rltk::to_cp437('>');
             fg = RGB::from_f32(0.0, 1.0, 1.0);
+        }
+        TileType::Bridge => {
+            glyph = rltk::to_cp437('.');
+            fg = RGB::named(rltk::CHOCOLATE);
+        }
+        TileType::Road => {
+            glyph = rltk::to_cp437('~');
+            fg = RGB::named(rltk::GRAY);
+        }
+        TileType::Grass => {
+            glyph = rltk::to_cp437('"');
+            fg = RGB::named(rltk::GREEN);
+        }
+        TileType::ShallowWater => {
+            glyph = rltk::to_cp437('≈');
+            fg = RGB::named(rltk::CYAN);
+        }
+        TileType::DeepWater => {
+            glyph = rltk::to_cp437('≈');
+            fg = RGB::named(rltk::NAVY_BLUE);
         }
     }
     if map.visible_tiles[idx] {
---
+++ src/map/tile_type.rs
@@ -0,0 +1,55 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub enum TileType {
+    Wall,
+    Floor,
+    DownStairs,
+    Road,
+    Grass,
+    ShallowWater,
+    DeepWater,
+    WoodFloor,
+    Bridge,
+}
+
+pub fn tile_walkable(tt: TileType) -> bool {
+    match tt {
+        TileType::Floor
+        | TileType::DownStairs
+        | TileType::Road
+        | TileType::Grass
+        | TileType::ShallowWater
+        | TileType::WoodFloor
+        | TileType::Bridge => true,
+        TileType::Wall | TileType::DeepWater => false,
+    }
+}
+
+pub fn tile_opaque(tt: TileType) -> bool {
+    match tt {
+        TileType::Wall => true,
+        TileType::Floor
+        | TileType::DownStairs
+        | TileType::Road
+        | TileType::Grass
+        | TileType::ShallowWater
+        | TileType::DeepWater
+        | TileType::WoodFloor
+        | TileType::Bridge => false,
+    }
+}
+
+pub fn tile_cost(tt: TileType) -> f32 {
+    match tt {
+        TileType::Road => 0.8,
+        TileType::Grass => 1.1,
+        TileType::ShallowWater => 1.2,
+        TileType::Wall
+        | TileType::Floor
+        | TileType::DownStairs
+        | TileType::DeepWater
+        | TileType::WoodFloor
+        | TileType::Bridge => 1.0,
+    }
+}
