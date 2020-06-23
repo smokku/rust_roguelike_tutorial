5f0cde068f2a084b4b2bb47184a74bb76081032e
Tomasz Sterna <tomek@sterna.link>
2020-06-23 15:29:48 +0200

    Adding town walls, gravel and a road

--- src/map_builders/town.rs
+++ src/map_builders/town.rs
@@ -2,6 +2,7 @@ use super::{
     AreaStartingPosition, BuilderChain, BuilderMap, DistantExit, InitialMapBuilder, TileType,
 };
 use rltk::RandomNumberGenerator;
+use std::collections::HashSet;
 
 pub fn town_builder(
     depth: i32,
@@ -35,12 +36,15 @@ impl TownBuilder {
         rng: &mut rltk::RandomNumberGenerator,
         build_data: &mut BuilderMap,
     ) {
+        self.grass_layer(build_data);
+        self.water_and_piers(rng, build_data);
+        let (mut available_building_tiles, wall_gap_y) = self.town_walls(rng, build_data);
+
         // Make visible for screenshot
         for t in build_data.map.visible_tiles.iter_mut() {
             *t = true;
         }
-        self.grass_layer(build_data);
-        self.water_and_piers(rng, build_data);
+        build_data.take_snapshot();
     }
 
     fn grass_layer(&mut self, build_data: &mut BuilderMap) {
@@ -78,5 +82,46 @@ impl TownBuilder {
             }
             build_data.take_snapshot();
         }
+    }
+
+    fn town_walls(
+        &mut self,
+        rng: &mut RandomNumberGenerator,
+        build_data: &mut BuilderMap,
+    ) -> (HashSet<usize>, i32) {
+        let mut available_building_tiles = HashSet::new();
+        let wall_gap_y = rng.roll_dice(1, build_data.map.height - 9) + 5;
+        for y in 1..build_data.map.height - 2 {
+            if !(y > wall_gap_y - 4 && y < wall_gap_y + 4) {
+                let idx = build_data.map.xy_idx(30, y);
+                build_data.map.tiles[idx] = TileType::Wall;
+                build_data.map.tiles[idx - 1] = TileType::Floor;
+                let idx_right = build_data.map.xy_idx(build_data.map.width - 2, y);
+                build_data.map.tiles[idx_right] = TileType::Wall;
+                for x in 31..build_data.map.width - 2 {
+                    let gravel_idx = build_data.map.xy_idx(x, y);
+                    build_data.map.tiles[gravel_idx] = TileType::Gravel;
+                    if y > 2 && y < build_data.map.height - 1 {
+                        available_building_tiles.insert(gravel_idx);
+                    }
+                }
+            } else {
+                for x in 30..build_data.map.width {
+                    let road_idx = build_data.map.xy_idx(x, y);
+                    build_data.map.tiles[road_idx] = TileType::Road;
+                }
+            }
+        }
+        build_data.take_snapshot();
+
+        for x in 30..build_data.map.width - 1 {
+            let idx_top = build_data.map.xy_idx(x, 1);
+            build_data.map.tiles[idx_top] = TileType::Wall;
+            let idx_bottom = build_data.map.xy_idx(x, build_data.map.height - 2);
+            build_data.map.tiles[idx_bottom] = TileType::Wall;
+        }
+        build_data.take_snapshot();
+
+        (available_building_tiles, wall_gap_y)
     }
 }
