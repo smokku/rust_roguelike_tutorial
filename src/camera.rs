332f5399f32ce0572f5a4db45e975ab5dfb666c4
Tomasz Sterna <tomek@sterna.link>
2020-06-23 14:33:27 +0200

    Add Gravel tile type

--- src/camera.rs
+++ src/camera.rs
@@ -125,7 +125,7 @@ fn get_tile_glyph(idx: usize, map: &Map) -> (rltk::FontCharType, RGB, RGB) {
             fg = RGB::from_f32(0.0, 0.5, 0.5);
         }
         TileType::WoodFloor => {
-            glyph = rltk::to_cp437('.');
+            glyph = rltk::to_cp437('░');
             fg = RGB::named(rltk::CHOCOLATE);
         }
         TileType::Wall => {
@@ -143,7 +143,7 @@ fn get_tile_glyph(idx: usize, map: &Map) -> (rltk::FontCharType, RGB, RGB) {
             fg = RGB::named(rltk::CHOCOLATE);
         }
         TileType::Road => {
-            glyph = rltk::to_cp437('~');
+            glyph = rltk::to_cp437('≡');
             fg = RGB::named(rltk::GRAY);
         }
         TileType::Grass => {
@@ -151,12 +151,16 @@ fn get_tile_glyph(idx: usize, map: &Map) -> (rltk::FontCharType, RGB, RGB) {
             fg = RGB::named(rltk::GREEN);
         }
         TileType::ShallowWater => {
-            glyph = rltk::to_cp437('≈');
+            glyph = rltk::to_cp437('~');
             fg = RGB::named(rltk::CYAN);
         }
         TileType::DeepWater => {
-            glyph = rltk::to_cp437('≈');
-            fg = RGB::named(rltk::NAVY_BLUE);
+            glyph = rltk::to_cp437('~');
+            fg = RGB::named(rltk::BLUE);
+        }
+        TileType::Gravel => {
+            glyph = rltk::to_cp437(';');
+            fg = RGB::named(rltk::GRAY);
         }
     }
     if map.visible_tiles[idx] {
